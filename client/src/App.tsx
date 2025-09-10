f36149a7031da532c888b992e6a4d2bcea5599fe
Lucas Ekstener <43437993+herr-ek@users.noreply.github.com>
2025-09-10 17:19:49 +0200

    Dynamic page loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,8 @@
-import { Route, Routes } from "react-router-dom";
-
-import AboutPage from "@pages/about";
-import BlogPage from "@pages/blog";
-import DocsPage from "@pages/docs";
-import IndexPage from "@pages/index";
-import PricingPage from "@pages/pricing";
+import { useRoutes } from "react-router-dom";
+import routes from "~react-pages";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
-  );
+  return useRoutes(routes);
 }
 
 export default App;
