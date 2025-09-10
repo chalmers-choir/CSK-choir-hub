d9501228865adba8037d82498503e85ba2a222d1
Lucas Ekstener <43437993+herr-ek@users.noreply.github.com>
2025-09-10 17:30:35 +0200

    Initialized a next js client app instead of vite. Vite client renamed to client legacy

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,23 @@
 import {heroui} from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
-    "./index.html",
-    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
 }
+
+export default config;
