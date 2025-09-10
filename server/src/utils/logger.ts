05ff3e60ec9766c19c682494602b0683b44b9ab7
Liam <37960435+liamcco@users.noreply.github.com>
2025-09-10 23:45:15 +0200

    custom logger

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,5 +1,14 @@
 import winston from 'winston';
 
+interface CustomLogger extends winston.Logger {
+  infoObject: (obj: any, message?: string) => void;
+  withContext: (context: string) => {
+    info: (msg: string) => void;
+    error: (msg: string) => void;
+    debug: (msg: string) => void;
+  };
+}
+
 const { combine, timestamp, printf, colorize } = winston.format;
 
 // Custom log format
@@ -18,6 +27,20 @@ const logger = winston.createLogger({
         new winston.transports.Console(),
         new winston.transports.File({ filename: 'logs/combined.log', format: winston.format.uncolorize() }),
     ],
-});
+}) as CustomLogger;
+
+logger.infoObject = function(obj: any, message?: string) {
+    const msg = message ? `${message} ${JSON.stringify(obj)}` : JSON.stringify(obj);
+    this.info(msg);
+};
+
+logger.withContext = function(context: string) {
+    const prefix = `[${context}] `;
+    return {
+        info: (msg: string) => logger.info(prefix + msg),
+        error: (msg: string) => logger.error(prefix + msg),
+        debug: (msg: string) => logger.debug(prefix + msg),
+    };
+};
 
 export default logger;
