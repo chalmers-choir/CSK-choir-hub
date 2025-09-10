30ca4a8d5f7ab46a65721d131d3185d0853ac726
Liam <37960435+liamcco@users.noreply.github.com>
2025-09-10 21:34:23 +0200

    simplified health check

--- server/src/api/controllers/healthController.ts
+++ server/src/api/controllers/healthController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const health = async (req: Request, res: Response) => {
-    return res.status(200).json({ "status": "ok" });
+    return res.status(200);
 };
