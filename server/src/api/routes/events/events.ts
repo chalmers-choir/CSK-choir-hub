0c0223e87b6d351740d60938aeb967602dc70146
Liam <37960435+liamcco@users.noreply.github.com>
2025-09-11 22:14:43 +0200

    reordered routes

--- server/src/api/routes/events/events.ts
+++ server/src/api/routes/events/events.ts
@@ -13,10 +13,11 @@ const router = Router();
 
 router.get('/', getEvents);
 router.post('/', requireAuth({ roles: ['admin'] }), createEvent);
-router.delete('/:id', requireAuth({ roles: ['admin'] }), deleteEvent);
-router.put('/:id', requireAuth({ roles: ['admin'] }), updateEvent);
 
 router.use('/attendance', requireAuth(), attendanceRoutes);
 router.use('/registration', requireAuth(), registrationRoutes);
+
+router.delete('/:id', requireAuth({ roles: ['admin'] }), deleteEvent);
+router.put('/:id', requireAuth({ roles: ['admin'] }), updateEvent);
 
 export default router;
--- server/src/api/routes/groups/groups.ts
+++ server/src/api/routes/groups/groups.ts
@@ -13,9 +13,9 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', getGroups);
-router.delete('/:groupId', requireAuth({ roles: ['admin'] }), deleteGroup);
 router.post('/', requireAuth({ roles: ['admin'] }), createGroup);
 router.put('/:groupId', requireAuth({ roles: ['admin'] }), updateGroup);
+router.delete('/:groupId', requireAuth({ roles: ['admin'] }), deleteGroup);
 
 router.use('/:groupId/groups', requireAuth({ roles: ['admin'] }), groupsRouter);
 router.use('/:groupId/roles', requireAuth({ roles: ['admin'] }), rolesRouter);
