759e7d16c212b89b5df37029cf9d61925301360e
Liam <37960435+liamcco@users.noreply.github.com>
2026-02-24 17:15:17 +0100

    inital client commit

---
+++ client/lib/api-client/models/Registration.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type Registration = {
+    userId: number;
+    firstName: string;
+    lastName: string;
+    comments?: string;
+    dietaryRestrictions?: string;
+};
+
---
+++ client/lib/api-client/models/User.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { GroupSummary } from './GroupSummary';
+import type { KnownSong } from './KnownSong';
+import type { RoleSummary } from './RoleSummary';
+export type User = {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    groups: Array<GroupSummary>;
+    roles: Array<RoleSummary>;
+    knownSongs: Array<KnownSong>;
+    dietaryPreferences?: string;
+};
+
