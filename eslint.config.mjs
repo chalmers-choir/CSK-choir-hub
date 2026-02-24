e9fccf5ec5c23a7382bfd67774c899f99b1c8d4c
Liam <37960435+liamcco@users.noreply.github.com>
2026-02-24 14:43:38 +0100

    lint runs with NO warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { fixupPluginRules } from "@eslint/compat";
 import js from "@eslint/js";
 import nextPlugin from "@next/eslint-plugin-next";
