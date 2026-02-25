5166ec4edd3f54ec05d6146447c5a9888cdd29ad
Liam <37960435+liamcco@users.noreply.github.com>
2026-02-25 18:40:28 +0100

    Event overview (#27)
    * event detail card
    
    * update to use generated client
    
    * feat: add translation to event overview
    
    * chore: break UI up into components
    
    * style: move week dates
    
    * move week to common
    
    * remove redundant translation

---
+++ client/components/events/EventsWeekSections.tsx
@@ -0,0 +1,38 @@
+import { EventListCard } from "./EventListCard";
+import { useTranslation } from "@/contexts/IntlContext";
+import { CSKEvent } from "@/lib/apiClient";
+
+interface EventWeekGroup {
+  key: string;
+  weekNumber: number;
+  rangeLabel: string;
+  items: CSKEvent[];
+}
+
+interface EventsWeekSectionsProps {
+  weeks: EventWeekGroup[];
+}
+
+export const EventsWeekSections = ({ weeks }: EventsWeekSectionsProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex flex-col gap-8">
+      {weeks.map((week) => (
+        <div key={week.key} className="flex flex-col gap-3">
+          <div className="flex items-baseline gap-2">
+            <h2 className="text-default-900 text-xl font-semibold">
+              {t("utils.week")} {week.weekNumber}
+            </h2>
+            <span className="text-default-500 text-sm">{week.rangeLabel}</span>
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {week.items.map((event) => (
+              <EventListCard key={event.id} event={event} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
