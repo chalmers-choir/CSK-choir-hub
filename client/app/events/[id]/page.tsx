8aa49817b3524ae7f2ac2f232684b4f99e7c286c
Liam <37960435+liamcco@users.noreply.github.com>
2026-02-25 18:42:46 +0100

    update name of page

--- client/app/events/[id]/page.tsx
+++ client/app/events/[id]/page.tsx
@@ -2,7 +2,7 @@ import EventDetailCard from "@/components/events/EventDetailCard";
 import DefaultLayout from "@/layouts/default";
 import { EventsService } from "@/lib/api-client";
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
+export default async function EventDetailPage(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
 
