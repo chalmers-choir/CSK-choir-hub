dc1b6524930b8ca04049925084453e080d57bd04
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-06 18:25:52 +0100

    enable mobile development

--- client/lib/apiClient.ts
+++ client/lib/apiClient.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { siteConfig } from '@/config/site';
 
 import { OpenAPI } from './api-client/core/OpenAPI';
@@ -7,3 +9,16 @@ export * from './api-client';
 OpenAPI.BASE = siteConfig.apiBaseUrl;
 OpenAPI.WITH_CREDENTIALS = true;
 OpenAPI.CREDENTIALS = 'include';
+
+OpenAPI.BASE = (() => {
+  if (process.env.NODE_ENV === 'development') {
+    if (typeof window !== 'undefined') {
+      const protocol = window.location.protocol;
+      const hostname = window.location.hostname;
+
+      return `${protocol}//${hostname}:5050/api`;
+    }
+  }
+
+  return siteConfig.apiBaseUrl;
+})();
